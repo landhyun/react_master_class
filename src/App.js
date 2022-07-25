64cb0e6f42e9f82b0ec525db11d11b598f8ab1d1
landhyun <wlgusdbr22@naver.com>
2022-07-25 16:41:55 +0900

    #2.3 'as', attr({ 필수조건 })

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ const Father = styled.div`
     display: flex;
 `;
 
-const Box = styled.div`
-    background-color: ${props => props.bgColor};
-    width: 100px;
-    height: 100px;
+const Input = styled.input.attrs({required: true, maxLength: 5})`
+    color: white;
+    background-color: tomato;
+    border: 0;
+    border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-    border-radius: 50px;
-`;
 
-    
 function App() {
     return (
-        <Father>
-            <Box bgColor="teal" />
-            <Circle bgColor="whitesmoke" />
+        <Father as="header">
+            <Input />
         </Father>
     );
 }
